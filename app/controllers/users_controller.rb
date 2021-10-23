6365a1fc741269bf965b1e8e384ba12f789a9204
mapy0 <90832266+mapy0@users.noreply.github.com>
2021-10-23 17:33:12 +0000

    [update]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @user = current_user #User部分テンプレート
+    @new_book = Book.new #Book投稿部分テンプレート
+
   end
   
   #投稿リストを表示画面作成
